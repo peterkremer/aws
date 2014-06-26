da88cbcf85b6d0359828178767f1a6adc1b67292
Peter Kremer <pkremer3@u.rochester.edu>
2014-06-26 10:00:38 -0400

    mongohq now outputs mturk_data.csv containing data from ideation

--- mturk/mongohq.py
+++ mturk/mongohq.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+
+import sys
 # Requires pymongo
 import pymongo
-
+from datetime import datetime
 from pymongo import MongoClient
 
 
@@ -16,7 +19,13 @@ ideagens = {'url': "kahana.mongohq.com",
             'port': 10075,
             'dbName': 'IdeaGens',
             'user': 'experimenter',
-            'pswd': 'protolab1' }
+            'pswd':  sys.argv[1]}
+#
+ideagens_sandbox = {'url': "kahana.mongohq.com",
+                    'port': 10068,
+                    'dbName': 'ideagens_sandbox',
+                    'user': 'peter',
+                    'pswd': 'proto' }
 
 
 def get_mongodb(dbUrl, dbPort, dbName, dbUser, dbPswd):
@@ -28,23 +37,58 @@ def get_mongodb(dbUrl, dbPort, dbName, dbUser, dbPswd):
   dbURI = "mongodb://" + dbUser + ":" + dbPswd + "@" + dbUrl + ":" + \
       str(dbPort) + "/" + dbName
   client = MongoClient(dbURI)
+  print dbURI
   return client[dbName]
 
   
 if __name__ == '__main__':
-  dbUrl = "kahana.mongohq.com"
+  """dbUrl = "kahana.mongohq.com"
   dbPort = '10056'
   dbName = 'IdeaGensTest'
   dbUser = 'sandbox'
-  dbPswd = 'protolab1'
-  db = get_mongodb(ideagenstest['url'],
-                   ideagenstest['port'],
-                   ideagenstest['dbName'], 
-                   ideagenstest['user'],
-                   ideagenstest['pswd'])
+  dbPswd = 'protolab1' """
+  db = get_mongodb(ideagens['url'],
+                   ideagens['port'],
+                   ideagens['dbName'], 
+                   ideagens['user'],
+                   ideagens['pswd'])
 
   parts = db.participants.find()
-  
-  for part in parts:
-      print "id: %s" % (part['_id'])
-      print "verification: %i" % (part['verifyCode'])
+  ideas = db.ideas.find()
+  events = db.events.find()
+
+  count = 0
+  mturk_data = open('mturk_data.csv', "w")
+
+  ideation_starts = []
+  for event in events:
+    part_id = event['participant']['_id']
+    event_desc = event['description']
+    event_time = event['time']
+    if (event_desc == "Participant began ideation"):
+      ideation_starts.append([part_id, event_time])
+
+  start_times = dict(ideation_starts)
+
+  mturk_data = open('mturk_data.csv', "w")
+  mturk_data.write("part_ID, part_prime, idea_ID, idea_content, idea_time, ideation_start, rel_idea_time \n")
+  for idea in ideas:
+    prompt_ID = idea['prompt']['_id']
+
+    part_ID = idea['participant']['_id']
+
+    part_prime = idea['participant']['condition']['description'].split()
+    part_prime = " ".join(part_prime[4:5])
+
+    idea_ID = idea['_id']
+
+    idea_content = idea['content'].replace(', ', '; ').replace('\n', ' ')
+
+    idea_time = idea['time']
+    idea_time = datetime.utcfromtimestamp(idea_time/1000.0)
+
+    ideation_start_time = start_times[part_ID]
+
+    rel_idea_time = idea_time - ideation_start_time
+    mturk_data.write("%s, %s, %s, %s, %s, %s, %s \n" % (part_ID, part_prime, idea_ID, idea_content, idea_time, ideation_start_time, rel_idea_time))
+  mturk_data.close()
